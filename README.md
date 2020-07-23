8de1f3cd8f75887a6fe849cb1ffecd0408f39937
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-07-22 20:52:30 -0700

    updated models

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Brain-Games-Sequelized
-The Brain Games App Utilizing Sequelize ORM
+### by Steffi Wellington
+The [Brain Games](https://enigmatic-sea-12625.herokuapp.com/) App Utilizing Sequelize ORM
+
+## Technologies
+Technologies Used
+1. package.json
+2. node.js
+3. express.js
+4. express-handlebars.js
+5. body-parser.js
+6. MySQL
+7. .gitignore
+8. .env
+9. Sequelize ORM
+
+Local DB Created
+Jaws db Connection Created 
+Jaws db Table Created with insertions
--- models/NewPuzzle.js
+++ models/NewPuzzle.js
@@ -1,23 +1,7 @@
-var orm = require("../config/orm.js");
-
-var Puzzle = {
-    all: function(cb) {
-        orm.all("puzzles", function(res) {
-            cb(res);
-        });
-    },
-    // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
-        orm.create("puzzles", cols, vals, function(res) {
-            cb(res);
-        });
-    },
-    update: function(objColVals, condition, cb) {
-        orm.update("puzzles", objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
+module.exports = function (sequelize, DataTypes) {
+    var Puzzle = sequelize.define("Puzzle", {
+        text: DataTypes.STRING,
+        complete: DataTypes.BOOLEAN
+    });
+    return Puzzle;
 };
-
-// Export the database functions for the controller.
-module.exports = Puzzle;
